a5471b7fccd59f9f9a9efa50fae0320779ea329a
TonyH98 <tonyhoang@pursuit.org>
2022-08-30 13:03:24 -0400

    Form is complete

---
+++ index.js
@@ -0,0 +1,26 @@
+const header = document.createElement("header")
+const form = document.createElement("form")
+const h1 = document.createElement("h1")
+h1.textContent = "Weather App"
+header.append(form)
+form.append(h1)
+const body = document.querySelector("body")
+body.append(header)
+const input = document.createElement("input")
+input.setAttribute("type", "text")
+input.setAttribute("id", "text")
+input.setAttribute("name", "text")
+const label = document.createElement("label")
+label.setAttribute("for", "text")
+label.textContent = "Pick a Location "
+const button = document.createElement("button")
+button.setAttribute("type", "submit")
+button.textContent = "Get Weather"
+form.append(label, input, button)
+
+const div = document.createElement("div")
+body.append(div)
+div.setAttribute("class", "info")
+const main = document.createElement("main")
+const aside = document.createElement("aside")
+div.append(main, aside)
