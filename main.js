be71a97d36fff71a1d72e2866798963bbcdfdaf2
MarsiaRochelin <marsrochelin@pursuit.org>
2022-08-30 17:33:48 -0400

    HTML update

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ const form = document.querySelector('form')
 
 const main = document.querySelector('main')
 
-
-
 // const sidebar = document.querySelector('aside')
 
 // const section = document.querySelector('section')
