d95af4b3af25274882306ab585bbb884e725698a
Jonathan-Gaal <jonathangaal@pursuit.org>
2022-08-30 21:21:25 -0400

    got all 3 day forcast items displaying

--- script.js
+++ script.js
@@ -1,36 +1,101 @@
+// User Input Box, Header Form, Main
 const cityInputBox = document.querySelector(`.cityInputBox`);
 const headerForm = document.querySelector(`.headerForm`);
 const chooseWeatherMain = document.querySelector(`.chooseWeather`);
+// 3 Day Forcast
+
+// grabs the 3 day forcast articles with the classlist of hidden
+const hidden = document.querySelector(`.hidden`);
+
+let day1AvgTemp = document.querySelector(`#day1AvgTemp`);
+console.log(day1AvgTemp)
+let day1MaxTemp = document.querySelector(` #day1MaxTemp`);
+let day1MinTemp = document.querySelector(`#day1MinTemp`);
+
+
+let day2AvgTemp = document.querySelector(`#day2AvgTemp`);
+let day2MaxTemp = document.querySelector(`#day2MaxTemp`);
+let day2MinTemp = document.querySelector(`#day2MinTemp`);
+
+
+let day3AvgTemp = document.querySelector(`#day3AvgTemp`);
+let day3MaxTemp = document.querySelector(`#day3MaxTemp`);
+let day3MinTemp = document.querySelector(`#day3MinTemp`);
+
+
+//grbs individual days
+// const forcastAside = document.querySelector(`.forcastAside`)
+// const day1 = document.querySelector(`#day1`);
+// const day2 = document.querySelector(`#day2`);
+// const day3 = document.querySelector(`#day3`);
 
 
 //Add submit event listener to headerForm
 headerForm.addEventListener(`submit`, (e) => {
     e.preventDefault();
-
+    
     //turns input.value into a variable, trims it, and removes spaces and adds + char to make it URL-ready
     const cityInputVal = cityInputBox.value.trim().split(" ").join("+");
-        
+      
     //inserts user input string into the URL to complete a fetch call
     let URL = `https://wttr.in/${cityInputVal}?format=j1`; //--> "New+York"
     
     //FETCH
   fetch(URL)
     .then((res) => res.json())
-    .then((resJson) => {
-      let current = resJson.current_condition[0];
-      let area = resJson.nearest_area[0];
-      console.log(area); // works, not sure why it wont print in statement
+      .then((resJson) => {
+        
+          //Weather Conditions
+          let current = resJson.current_condition[0];
+          //Areas
+          let area = resJson.nearest_area[0];
+          //Weather -- for 3 Day Forcast Aside
+          let day1 = resJson.weather[0]; 
+          let day2 = resJson.weather[1];
+          let day3 = resJson.weather[2];
+          
+      console.log(day1); 
 
       let areaName = area.areaName[0].value; //--> area.areaName
       let region = area.region[0].value; //--> area.region
       let country = area.country[0].value; //--> area.country
-        let feels_LikeF = current.FeelsLikeF; //--> currentcurrent_condition
-        
-    //   console.log(country)
+          let feels_LikeF = current.FeelsLikeF; //--> currentcurrent_condition
+    
+          
 
-      if (cityInputBox.value !== "") {
+    
+            // IF Statement that fires via submit event IF Input Box has information it. Displays output for weather and regional info.
+      if (cityInputBox.value !== "" && resJson !== undefined) {
         chooseWeatherMain.innerHTML = `<h2>${cityInputBox.value}</h2>\n<strong>Area:</strong><p>${areaName}</p>\n<strong>Region:</strong><p>${region}</p>\n<strong>Country:<p>${country}</p></strong>\n<strong>Currently:</strong> <p>Feels Like ${feels_LikeF} F</p>`;
-      }
+
+        // sets 3 day forcast variables to values from resJson
+
+        day1AvgTemp.innerText = `${day1.avgtempF}`;
+        day1MaxTemp.innerText = `${day1.maxtempF}`;
+        day1MinTemp.innerText = `${day1.mintempF}`;
+        
+
+        day2AvgTemp.innerText = `${day2.avgtempF}`;
+        day2MaxTemp.innerText = `${day2.maxtempF}`;
+        day2MinTemp.innerText = `${day2.mintempF}`;
+       
+
+        day3AvgTemp.innerText  = `${day3.mintempF}`;
+        day3MaxTemp.innerText  = `${day3.mintempF}`;
+        day3MinTemp.innerText  = `${day3.mintempF}`;
+          
+
+        hidden.classList.remove(`hidden`);
+
+        //HAS TO GO INTO A FUNCTION -- sets the 3 day forcast aside articles
+        //   day1.innerHTML = `<h2>Today</h2>\n</p>\n<strong>Average Temperature:</strong><p>${day1.avgtempF}</p>\n<strong>Max Temperature:<p>${day1.mintempF}</p></strong>\n<strong>Min Temperature:</strong> <p>Feels Like ${day1.maxtempF} F</p>`;
+
+        //   day2.innerHTML = `<h2>Today</h2>\n</p>\n<strong>Average Temperature:</strong><p>${day2.avgtempF}</p>\n<strong>Max Temperature:<p>${day2.mintempF}</p></strong>\n<strong>Min Temperature:</strong> <p>Feels Like ${day2.maxtempF} F</p>`;
+
+        //   day3.innerHTML = `<h2>Today</h2>\n</p>\n<strong>Average Temperature:</strong><p>${day3.avgtempF}</p>\n<strong>Max Temperature:<p>${day3.mintempF}</p></strong>\n<strong>Min Temperature:</strong> <p>Feels Like ${day3.maxtempF} F</p>`;
+      } //else {
+    //       console.error(`We could not find any city matching your input! Please Input a city name (with or without a country name) seperated by spaces.  `)
+      
         
    
         
