e01a26f45692778621f1707ee3db4263ed891834
Gillesedgy <edgygilles@pursuit.org>
2022-08-31 12:56:23 -0400

    5 test passed

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
-
-const form = document.querySelector("form")
-
-//!
-form.addEventListener("submit", ()=>{
-    event.preventDefault()
-
-
+const Base_URL = 'https://wttr.in/London'
+const form1 = document.querySelector("#search-bar")
+// console.log(form1)
+//search bar
+const searchBar= document.querySelector(".enter-location")
+// console.log(searchBar)
+form1.addEventListener("submit", (event)=>{
+ event.preventDefault()  
+ form1.reset()
+console.log("I have been reset")
+// let location = event.target.location.value
+// console.log(location)
+fetch("https://wttr.in/London")
+.then((res)=>res.json)
+.then((resJson)=>{
+    console.log(resJson.results)
+    let results = resJson.results
 })
+.catch((err)=> err)
+})
+//!
+// form1.addEventListener("submit", (event)=>{
+//     event.preventDefault()
+// searchBar.addEventListener("submit", (e)=>{
+//     searchBar.textContent
+// })
+
+// })
