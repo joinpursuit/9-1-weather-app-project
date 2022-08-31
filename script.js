b33ec2ab0ec5dac94adee8a187ecdf71f974be74
Salinamalek <salinamalek@pursuit.org>
2022-08-31 13:15:39 -0400

    temp conversion radio buttons added

---
+++ script.js
@@ -0,0 +1,20 @@
+const BASE_URL = "https://wttr.in/"
+
+const form = document.querySelector('form');
+const main = document.querySelector('main');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    // document.querySelector('main p').hidden = true;
+    // let userTextInput = event.target.location.value;
+    // event.target.location.value = "";
+
+    // fetch('${BASE_URL}')
+    // .then((res) => res.json())
+    // .then((res2) => {
+    //     res2.results.
+    // })
+    // .catch()
+
+})
