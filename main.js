15b5b855b84802ccc99a9899a7d38e2a8cd15ab2
Gillesedgy <edgygilles@pursuit.org>
2022-08-31 17:24:02 -0400

    fixed RUL ISSE and working on After Search Section

--- main.js
+++ main.js
@@ -5,24 +5,42 @@ const form = document.querySelector("form");
 const today = document.querySelector("#Today")
 const tomorrow = document.querySelector("#Tomorrow")
 const dayAfter= document.querySelector("#Day After Tomotrrow")
+const current = document.querySelector(".current-weather")
 const ul = document.querySelector("ul")
 // console.log(form1)
 //search bar
 // console.log(searchBar)
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  //   form.reset();
+    form.reset();
   const city = bar.value.split(" ").join("+"); //?
-  console.log(city);
+//   console.log(city);
   let Base_URL = `http://wttr.in/${city}?format=j1`;
 
   fetch(Base_URL)
     .then((res) => res.json())
-
     .then((resJson) => {
-      console.log(resJson);
-    //   let results = resJson.results;
-      console.log(resJson.current_condition[0].FeelsLikeF);
+        console.log(resJson)
+        const areaP = document.createElement("p")
+        let area = resJson.nearest_area[0].areaName[0].value
+        areaP.textContent= `${area}`
+        console.log(areaP);
+current.append(areaP)
+
+        const regionP = document.createElement("p")
+        let region = resJson.nearest_area[0].region[0].value
+        regionP.textContent=`${region}`
+        current.append(regionP)
+
+     const countryP = document.createElement("p")
+    const country = resJson.nearest_area[0].country[0].value
+    current.append(countryP)
+
+    const currentlyP = document.createElement("p")
+        const currently = resJson.current_condition[0].FeelsLikeF
+        console.log(currentlyP)
+    current.append(currently)
+  
     })
     .catch((err) => err);
 });
