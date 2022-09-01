60d43a93d67cb452c83670b3f9c83719cbbaa284
Gillesedgy <edgygilles@pursuit.org>
2022-09-01 12:12:20 -0400

    working on finishing main section

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const searchBar = document.querySelector(".enter-location");
 const bar = document.querySelector("#enter-location");
 // console.log(bar.value);
 const form = document.querySelector("form");
-const threeDays = document.querySelector("#threeDays")
+const threeDays = document.querySelector("#threeDays");
 const title = document.querySelector("main article");
 
 const ul = document.querySelector("ul");
@@ -11,24 +11,22 @@ const current = document.querySelector(".current-weather");
 //search bar
 // console.log(searchBar)
 form.addEventListener("submit", (event) => {
-    const city = bar.value.split(" ").join("+"); //?
-    let Base_URL = `https://wttr.in/${city}?format=j1`;
+  const city = bar.value.split(" ").join("+"); //?
+  let Base_URL = `https://wttr.in/${city}?format=j1`;
   event.preventDefault();
   form.reset();
   //   console.log(city);
   fetch(Base_URL)
     .then((res) => res.json())
     .then((resJson) => {
-      console.log(resJson)
+      console.log(resJson);
       let area = resJson.nearest_area[0].areaName[0].value;
       //   console.log(areaP);
       if (resJson) {
         title.innerHTML = `<h2><b>${area}</b></h2>`; //!
       }
       // const weatherBut = document.querySelector("get-weather");
-      
-      
-      
+
       const areaP = document.createElement("p");
       areaP.innerHTML = `<b>Area:</b>${area}`;
       current.append(areaP);
@@ -40,7 +38,7 @@ form.addEventListener("submit", (event) => {
 
       const countryP = document.createElement("p");
       const country = resJson.nearest_area[0].country[0].value;
-      countryP.innerHTML = `<b>Country:</b> ${country}`;
+      countryP.innerHTML = `<strong>Country:</strong> ${country}`;
       current.append(countryP);
 
       const currentlyP = document.createElement("p");
@@ -48,13 +46,37 @@ form.addEventListener("submit", (event) => {
       currentlyP.innerHTML = `<b>Currently:</b>Feels Like ${currently}°F`;
       //   console.log(currentlyP);
       current.append(currentlyP);
-      
+
       const today = document.querySelector("#Today");
-      today.innerText="TODAY"
+      // today.innerText = "TODAY";
       const tomorrow = document.querySelector("#Tomorrow");
       const dayAfter = document.querySelector("#Day After Tomorrow");
+      
+//!THREDAYS
+      threeDays.append(today, tomorrow, dayAfter);
+      let avgTempF = resJson.weather[0].avgtempF;
+      console.log(avgTempF);
+      // let minTempF = resJson.weather[0].hourly[0].minTempF;
+      // console.log(minTempF);
 
-      threeDays.append(today, tomorrow, dayAfter)
+      //! Chance of rain, sunshine, snow
+      //sunshine
+      const sunShineP = document.createElement("p")
+      const sunShine = resJson.weather[0].hourly[0].chanceofsunshine
+      // console.log(sunShine)
+      sunShineP.innerHTML=`<strong>Chance Of Sunshine:</strong>${sunShine}`
+      current.append(sunShineP)
+//rain
+      const rainP = document.createElement("p")
+      const rain = resJson.weather[0].hourly[0].chanceofrain
+      rainP.innerHTML=`<strong>Chane Of Rain:</strong>${rain}`
+      console.log(rain)
+      current.append(rainP)
+//snow
+      const snowP = document.createElement("p")
+      const snow = resJson.weather[0].chanceofsnow
+      snowP.innerHTML=`<b>Chance of Rain:</b>${snow}`
+      //!
     })
     .catch((err) => err);
 });
