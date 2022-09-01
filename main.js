74831c7e4aacecda917432b0fa445f9e6645e0e6
minagrullon <minagrullon@pursuit.org>
2022-08-31 21:51:23 -0400

    fix url

--- main.js
+++ main.js
@@ -1,18 +1,50 @@
 const form = document.querySelector("form");
+const placeholder = document.querySelector(".placeholder");
 const cityInput = document.querySelector(".cityInput");
 const submit = document.querySelector(".toSubmit");
 const today = document.querySelector(".today");
 const tomorrow = document.querySelector(".tomorrow");
 const dayAfter = document.querySelector(".day-after");
-const URL = "wttr.in";
-const value = cityInput.value.joinn("");
+// const value = cityInput.value.split(" ").join("+");
+// const URL = "http://wttr.in";
+// console.log(cityInput.value);
 
 form.addEventListener("submit", (e) => {
-  fetch(`${URL}/${cvalue}?format=j1`)
+  let value = cityInput.value.split(" ").join("+");
+  let URL = `http://wttr.in/${value}?format=j1`;
+  console.log(cityInput.value);
+  e.preventDefault();
+  fetch(`${URL}`)
     .then((data) => data.json())
     .then((data) => {
-      const h31 = document.createElement("h3");
-      h31.innerText = "Today";
-      today.append(h31);
+      let cityName = data.nearest_area[0].areaName[0].value;
+
+      if (data) {
+        placeholder.innerHTML = `<h3><b>${cityName}</b></h3>`;
+      }
+      const area = document.createElement("p");
+      area.innerHTML = `<b>Area:</b> ${cityName}`;
+      placeholder.append(area);
+      console.log(data);
+      const region = document.createElement("p");
+      region.innerHTML = `<b>Region:</b> ${data.nearest_area[0].region[0].value}`;
+      placeholder.append(region);
+      const country = document.createElement("p");
+      country.innerHTML = `<b>Country:</b> ${data.nearest_area[0].country[0].value}`;
+      placeholder.append(country);
+      const currently = document.createElement("p");
+      currently.innerHTML = `<b>Currently:</b> Feels Like ${data.current_condition[0].FeelsLikeF}°F`;
+      placeholder.append(currently);
+
+      const h5_1 = document.createElement("h5");
+      h5_1.innerText = "Today";
+      today.append(h5_1);
+      const h5_2 = document.createElement("h5");
+      h5_2.innerText = "Tomorrow";
+      tomorrow.append(h5_2);
+      const h5_3 = document.createElement("h5");
+      h5_3.innerText = "Day After Tomorrow";
+      dayAfter.append(h5_3);
+      form.reset();
     });
 });
