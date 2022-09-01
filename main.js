17cf3d3402121143ea771e2f12262750f2e495b0
Gillesedgy <edgygilles@pursuit.org>
2022-08-31 20:40:53 -0400

    Search Section-Updated values and bolded

--- main.js
+++ main.js
@@ -2,53 +2,55 @@ const searchBar = document.querySelector(".enter-location");
 const bar = document.querySelector("#enter-location");
 // console.log(bar.value);
 const form = document.querySelector("form");
-const today = document.querySelector("#Today")
-const tomorrow = document.querySelector("#Tomorrow")
-const dayAfter= document.querySelector("#Day After Tomotrrow")
-const current = document.querySelector(".current-weather")
-const ul = document.querySelector("ul")
+const today = document.querySelector("#Today");
+const tomorrow = document.querySelector("#Tomorrow");
+const dayAfter = document.querySelector("#Day After Tomotrrow");
+const current = document.querySelector(".history");
+const ul = document.querySelector("ul");
+const curWeather = document.querySelector(".current-weather")
 // console.log(form1)
 //search bar
 // console.log(searchBar)
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-    form.reset();
-  const city = bar.value.split(" ").join("+"); //?
-//   console.log(city);
-  let Base_URL = `http://wttr.in/${city}?format=j1`;
-
+  form.reset();
+  const city = bar.value //?
+  //   console.log(city);
+  let Base_URL = `https://wttr.in/${city}?format=j1`;
+// let Base_URL2= 'http://wttr.in/Melbourne?format=j1'
   fetch(Base_URL)
     .then((res) => res.json())
     .then((resJson) => {
-        console.log(resJson)
-        const areaP = document.createElement("p")
-        let area = resJson.nearest_area[0].areaName[0].value
-        areaP.textContent= `${area}`
-        console.log(areaP);
-current.append(areaP)
+      console.log(resJson);
+      const weatherBut = document.querySelector("get-weather")
+      
+      const areaP = document.createElement("p");
+      let area = resJson.nearest_area[0].areaName[0].value;
+      areaP.innerHTML = `<b>Area:</b>${area}`;
+    //   console.log(areaP);
+      current.append(areaP);
 
-        const regionP = document.createElement("p")
-        let region = resJson.nearest_area[0].region[0].value
-        regionP.textContent=`${region}`
-        current.append(regionP)
+      const regionP = document.createElement("p");
+      let region = resJson.nearest_area[0].region[0].value;
+      regionP.innerHTML = `<b>Region:</b>  ${region}`;
+      current.append(regionP);
 
-     const countryP = document.createElement("p")
-    const country = resJson.nearest_area[0].country[0].value
-    current.append(countryP)
+      const countryP = document.createElement("p");
+      const country = resJson.nearest_area[0].country[0].value;
+      countryP.innerHTML = `<b>Country:</b> ${country}`;
+      current.append(countryP);
 
-    const currentlyP = document.createElement("p")
-        const currently = resJson.current_condition[0].FeelsLikeF
-        console.log(currentlyP)
-    current.append(currently)
-  
+      const currentlyP = document.createElement("p");
+      const currently = resJson.current_condition[0].FeelsLikeF;
+      currentlyP.innerHTML = `<b>Currently:</b>Feels Like ${currently}°F`;
+    //   console.log(currentlyP);
+      current.append(currentlyP);
+if(resJson){
+    curWeather.innerHTML = `<h2><b>${area}</b></h2>`
+}
+//  const today   
+    
     })
     .catch((err) => err);
 });
-//!
-// form1.addEventListener("submit", (event)=>{
-//     event.preventDefault()
-// searchBar.addEventListener("submit", (e)=>{
-//     searchBar.textContent
-// })
 
-// })
