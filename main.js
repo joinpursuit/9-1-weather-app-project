6d36d0baa26c1c81d16256636aaf7512bbd8ec37
minagrullon <minagrullon@pursuit.org>
2022-09-01 15:26:25 -0400

    temp info

--- main.js
+++ main.js
@@ -5,12 +5,14 @@ const submit = document.querySelector(".toSubmit");
 const today = document.querySelector(".today");
 const tomorrow = document.querySelector(".tomorrow");
 const dayAfter = document.querySelector(".day-after");
+const weatherInfo = document.querySelector(".weatherInfo");
+const tempStats = document.querySelectorAll(".weatherInfo article");
 // const value = cityInput.value.split(" ").join("+");
 // const URL = "http://wttr.in";
 // console.log(cityInput.value);
 
 form.addEventListener("submit", (e) => {
-  let value = cityInput.value.split(" ").join("+");
+  let value = cityInput.value;
   let URL = `http://wttr.in/${value}?format=j1`;
   console.log(cityInput.value);
   e.preventDefault();
@@ -18,33 +20,57 @@ form.addEventListener("submit", (e) => {
     .then((data) => data.json())
     .then((data) => {
       let cityName = data.nearest_area[0].areaName[0].value;
-
       if (data) {
-        placeholder.innerHTML = `<h3><b>${cityName}</b></h3>`;
+        placeholder.innerHTML = `<h2>${value}</h2>`;
+        weatherInfo.style.visibility = "visible";
       }
+
       const area = document.createElement("p");
-      area.innerHTML = `<b>Area:</b> ${cityName}`;
+      area.innerHTML = `<b>Nearest Area:</b> ${cityName}`;
       placeholder.append(area);
-      console.log(data);
+
       const region = document.createElement("p");
       region.innerHTML = `<b>Region:</b> ${data.nearest_area[0].region[0].value}`;
       placeholder.append(region);
+
       const country = document.createElement("p");
       country.innerHTML = `<b>Country:</b> ${data.nearest_area[0].country[0].value}`;
       placeholder.append(country);
+
       const currently = document.createElement("p");
       currently.innerHTML = `<b>Currently:</b> Feels Like ${data.current_condition[0].FeelsLikeF}°F`;
       placeholder.append(currently);
 
-      const h5_1 = document.createElement("h5");
-      h5_1.innerText = "Today";
-      today.append(h5_1);
-      const h5_2 = document.createElement("h5");
-      h5_2.innerText = "Tomorrow";
-      tomorrow.append(h5_2);
-      const h5_3 = document.createElement("h5");
-      h5_3.innerText = "Day After Tomorrow";
-      dayAfter.append(h5_3);
-      form.reset();
+      const sunshine = document.createElement("p");
+      sunshine.innerHTML = `<b>Chance of Sunshine:</b> ${data.weather[0].hourly[0].chanceofsunshine}`;
+      placeholder.append(sunshine);
+
+      const rain = document.createElement("p");
+      rain.innerHTML = `<b>Chance of Rain:</b> ${data.weather[0].hourly[0].chanceofrain}`;
+      placeholder.append(rain);
+
+      const snow = document.createElement("p");
+      snow.innerHTML = `<b>Chance of Snow:</b> ${data.weather[0].hourly[0].chanceofsnow}`;
+      placeholder.append(snow);
+
+      tempStats.forEach((day, i) => {
+        const av = document.createElement("p");
+        av.innerHTML = `<b>Average Temperature:</b> ${data.weather[i].avgtempF}`;
+        day.append(av);
+
+        const max = document.createElement("p");
+        max.innerHTML = `<b>Max Temperature:</b> ${data.weather[i].maxtempF}`;
+        day.append(max);
+
+        const min = document.createElement("p");
+        min.innerHTML = `<b>Min Temperature:</b> ${data.weather[i].mintempF}`;
+        day.append(min);
+      });
+
+      //   let avgTempF = data.weather[0].avgtempF;
+      //   console.log(avgTempF);
+      //   let minTempF = data.weather[0].mintempF;
+      //   console.log(minTempF);
     });
+  form.reset();
 });
