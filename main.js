36a8e89bac6cd9181e5fc93c10bf1edae0137fff
Gillesedgy <edgygilles@pursuit.org>
2022-09-01 15:25:17 -0400

    Working in 3days

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ const current = document.querySelector(".current-weather");
 //search bar
 // console.log(searchBar)
 form.addEventListener("submit", (event) => {
-  const city = bar.value.split(" ").join("+"); //?
+  const city = bar.value; //?
   let Base_URL = `https://wttr.in/${city}?format=j1`;
   event.preventDefault();
   form.reset();
@@ -23,12 +23,12 @@ form.addEventListener("submit", (event) => {
       let area = resJson.nearest_area[0].areaName[0].value;
       //   console.log(areaP);
       if (resJson) {
-        title.innerHTML = `<h2><b>${area}</b></h2>`; //!
+        title.innerHTML = `<h2><b>${city}</b></h2>`; //!
       }
       // const weatherBut = document.querySelector("get-weather");
 
       const areaP = document.createElement("p");
-      areaP.innerHTML = `<b>Area:</b>${area}`;
+      areaP.innerHTML = `<b>Nearest Area:</b>${area}`;
       current.append(areaP);
 
       const regionP = document.createElement("p");
@@ -50,33 +50,50 @@ form.addEventListener("submit", (event) => {
       const today = document.querySelector("#Today");
       // today.innerText = "TODAY";
       const tomorrow = document.querySelector("#Tomorrow");
-      const dayAfter = document.querySelector("#Day After Tomorrow");
-      
-//!THREDAYS
-      threeDays.append(today, tomorrow, dayAfter);
-      let avgTempF = resJson.weather[0].avgtempF;
-      console.log(avgTempF);
-      // let minTempF = resJson.weather[0].hourly[0].minTempF;
-      // console.log(minTempF);
+      const dayAfter = document.querySelector("#Day-After-Tomorrow");
 
+      //!THREEDAYS --> create a loop to go iterate and update the days. 
+      let days = ["Today", "Tomorrow", "Day After Tomorrow"]
+      for(let i=0; i< threeDays.length; i++){
+threeDays.innerHTML= ""
+        //avg
+        const avgP = document.createElement("p");
+        avgTempF = resJson.weather[i].avgtempF;
+        avgP.innerHTML = `<b>Average Temeprature</b>:${avgTempF}`;
+        //min
+        const minP = document.createElement("p");
+        let minTempF = resJson.weather[i].mintempF;
+        minP.innerHTML = `<strong>Min Temperature:</strong>${minTempF}`;
+        // max
+        const maxTempP = document.createElement("p");
+        let maxTempF = resJson.weather[i].maxtempF;
+        maxTempP.innerHTML = `<b>Max Temperature:${maxTempF}`;
+  
+        threeDays.append(avgP, maxTempP, minP);
+      }
       //! Chance of rain, sunshine, snow
       //sunshine
-      const sunShineP = document.createElement("p")
-      const sunShine = resJson.weather[0].hourly[0].chanceofsunshine
-      // console.log(sunShine)
-      sunShineP.innerHTML=`<strong>Chance Of Sunshine:</strong>${sunShine}`
-      current.append(sunShineP)
-//rain
-      const rainP = document.createElement("p")
-      const rain = resJson.weather[0].hourly[0].chanceofrain
-      rainP.innerHTML=`<strong>Chane Of Rain:</strong>${rain}`
-      console.log(rain)
-      current.append(rainP)
-//snow
-      const snowP = document.createElement("p")
-      const snow = resJson.weather[0].chanceofsnow
-      snowP.innerHTML=`<b>Chance of Rain:</b>${snow}`
+      const sunShineP = document.createElement("p");
+      const sunShine = resJson.weather[0].hourly[0].chanceofsunshine;
+      sunShineP.innerHTML = `<b>Chance of Sunshine:</b> ${sunShine}`;
+      current.append(sunShineP);
+      //rain
+      const rainP = document.createElement("p");
+      const rain = resJson.weather[0].hourly[0].chanceofrain;
+      rainP.innerHTML = `<strong>Chance of Rain:</strong>${rain}`;
+      current.append(rainP);
+      //snow
+      const snowP = document.createElement("p");
+      const snow = resJson.weather[0].hourly[0].chanceofsnow;
+      snowP.innerHTML = `<b>Chance of Snow:</b>${snow}`;
+      current.append(snowP);
       //!
+
+      // let searchValue =`${city}`
+      // // style.textAlign ="Right"
+      // const searchV = document.createElement("h3")
+      // searchV.innerHTML=`${city}`
+      // areaP.prepend(searchV)
     })
     .catch((err) => err);
 });
