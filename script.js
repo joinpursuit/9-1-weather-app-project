d38ad4c1beb49a4fc3614ae1cdef34eb9d358be4
Salinamalek <salinamalek@pursuit.org>
2022-09-02 13:35:15 -0400

    icons tests passed 15/22

--- script.js
+++ script.js
@@ -23,39 +23,54 @@ form.addEventListener('submit', (event) => {
 
         const area = res2.nearest_area[0].areaName[0].value;
         // console.log(area)
-        const areaP = document.createElement('p');
-        areaP.innerHTML = `<strong>Nearest Area: <strong/>${area}`;
-        current.append(areaP);
+        const areas = document.createElement('p');
+        areas.innerHTML = `<strong>Nearest Area: <strong/>${area}`;
+        current.append(areas);
 
         const region = res2.nearest_area[0].region[0].value;
-        const regionP = document.createElement('p');
-        regionP.innerHTML = `<strong>Region: <strong/>${region}`;
-        current.append(regionP);
+        const regions = document.createElement('p');
+        regions.innerHTML = `<strong>Region: <strong/>${region}`;
+        current.append(regions);
 
         const country = res2.nearest_area[0].country[0].value;
-        const countryP = document.createElement('p');
-        countryP.innerHTML = `<strong>Country: <strong/>${country}`;
-        current.append(countryP);
+        const countries = document.createElement('p');
+        countries.innerHTML = `<strong>Country: <strong/>${country}`;
+        current.append(countries);
 
         const feelsLike = res2.current_condition[0].FeelsLikeF;
         const feelsLikeP = document.createElement('p');
         feelsLikeP.innerHTML = `<strong>Currently: <strong/>Feels Like ${feelsLike}°F`;
         current.append(feelsLikeP);
 
-        const sunny = res2.weather[0].hourly[0].chanceofsunshine;
-        const sunnyP = document.createElement('p');
-        sunnyP.innerHTML = `<strong>Chance of Sunshine: <strong/>${sunny}`;
-        current.append(sunnyP);
+        const sun = res2.weather[0].hourly[0].chanceofsunshine;
+        const sunny = document.createElement('p');
+        sunny.innerHTML = `<strong>Chance of Sunshine: <strong/>${sun}`;
+        current.append(sunny);
 
-        const rainy = res2.weather[0].hourly[0].chanceofrain;
-        const rainyP = document.createElement('p');
-        rainyP.innerHTML = `<strong>Chance of Rain: <strong/>${rainy}`;
-        current.append(rainyP);
+        const rain = res2.weather[0].hourly[0].chanceofrain;
+        const rainy = document.createElement('p');
+        rainy.innerHTML = `<strong>Chance of Rain: <strong/>${rain}`;
+        current.append(rainy);
 
-        const snowy = res2.weather[0].hourly[0].chanceofsnow;
-        const snowyP = document.createElement('p');
-        snowyP.innerHTML = `<strong>Chance of Snow: <strong/>${snowy}`;
-        current.append(snowyP);
+        const snow = res2.weather[0].hourly[0].chanceofsnow;
+        const snowy = document.createElement('p');
+        snowy.innerHTML = `<strong>Chance of Snow: <strong/>${snow}`;
+        current.append(snowy);
+
+        const image = document.createElement('img');
+        if(sun > 50) {
+            image.setAttribute("src", './assets/icons8-summer.gif')
+            image.setAttribute("alt", "sun")
+            hiddenLocation.prepend(image);
+        } else if (rain > 50){
+            image.setAttribute("src", './assets/icons8-torrential-rain.gif')
+            image.setAttribute("alt", "rain")
+            hiddenLocation.prepend(image);
+        } else if (snow > 50) {
+            image.setAttribute("src", './assets/icons8-light-snow.gif')
+            image.setAttribute("alt", "snow")
+            hiddenLocation.prepend(image);
+        }
 
     })
     .catch((err) => console.log(err))
