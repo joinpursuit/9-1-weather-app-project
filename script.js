d1c2a3858a1a14c5349f28f5aff2dcfc46e79557
Salinamalek <salinamalek@pursuit.org>
2022-09-02 14:36:11 -0400

    3-day forecast test passed 16/22

--- script.js
+++ script.js
@@ -72,6 +72,44 @@ form.addEventListener('submit', (event) => {
             hiddenLocation.prepend(image);
         }
 
+        const today = document.querySelector('#today');
+        const avgTempToday = document.createElement('p');
+        const minTempToday = document.createElement('p');
+        const maxTempToday = document.createElement('p');
+        const avgTemp = res2.weather[0].avgtempF
+        avgTempToday.innerHTML = `<strong>Average Temperature: <strong/>${avgTemp}°F`;
+        const minTemp = res2.weather[0].mintempF
+        minTempToday.innerHTML = `<strong>Min Temperature: <strong/>${minTemp}°F`;
+        const maxTemp = res2.weather[0].maxtempF
+        maxTempToday.innerHTML = `<strong>Max Temperature: <strong/>${maxTemp}°F`;
+
+        today.append(avgTempToday, minTempToday, maxTempToday);
+
+        const tomorrow = document.querySelector('#tomorrow');
+        const avgTempTom = document.createElement('p');
+        const minTempTom = document.createElement('p');
+        const maxTempTom = document.createElement('p');
+        const avgTemp1 = res2.weather[1].avgtempF
+        avgTempTom.innerHTML = `<strong>Average Temperature: <strong/>${avgTemp1}°F`;
+        const minTemp1 = res2.weather[1].mintempF
+        minTempTom.innerHTML = `<strong>Min Temperature: <strong/>${minTemp1}°F`;
+        const maxTemp1 = res2.weather[1].maxtempF
+        maxTempTom.innerHTML = `<strong>Max Temperature: <strong/>${maxTemp1}°F`;
+
+        tomorrow.append(avgTempTom, minTempTom, maxTempTom);
+
+        const afterTmw = document.querySelector('#afterTomorrow');
+        const avgTempAfter = document.createElement('p');
+        const minTempAfter = document.createElement('p');
+        const maxTempAfter = document.createElement('p');
+        const avgTemp2 = res2.weather[2].avgtempF
+        avgTempAfter.innerHTML = `<strong>Average Temperature: <strong/>${avgTemp2}°F`;
+        const minTemp2 = res2.weather[2].mintempF
+        minTempAfter.innerHTML = `<strong>Min Temperature: <strong/>${minTemp2}°F`;
+        const maxTemp2 = res2.weather[2].maxtempF
+        maxTempAfter.innerHTML = `<strong>Max Temperature: <strong/>${maxTemp2}°F`;
+
+        afterTmw.append(avgTempAfter, minTempAfter, maxTempAfter);
     })
     .catch((err) => console.log(err))
 
