e8b13633d8dcbb839064bd53c54a458b7be070f1
TonyH98 <tonyhoang@pursuit.org>
2022-09-02 11:29:01 -0400

    8 Tests Passed

--- index.js
+++ index.js
@@ -27,12 +27,20 @@ main.setAttribute("id", "city")
 const aside = document.createElement("aside")
 div.append(main, aside)
 
+const section = document.createElement("section")
+
+
 const h4 = document.createElement("h4")
 h4.textContent = "Previous Searches"
 
 const ul = document.createElement("ul")
 
-aside.append(h4, ul)
+const paragraph = document.createElement("p")
+paragraph.textContent = `No previous searches`
+aside.append(section)
+section.append(h4, paragraph, ul)
+// paragraph.setAttribute("class", "hidden")
+
 
 const h2 = document.createElement("h2")
 h2.textContent = `Choose a location to view the weather`
@@ -47,8 +55,8 @@ const p3 = document.createElement("p")
 
 
 
-const section = document.createElement("section")
-section.setAttribute("id", "current")
+const section2 = document.createElement("section")
+section2.setAttribute("id", "current")
 
 const aside2 = document.createElement("aside")
 aside2.setAttribute("id", "forecast")
@@ -88,96 +96,106 @@ const pa3 = document.createElement("p")
 div4.append(anotherh3, pa1, pa2, pa3)
 
 const p4 = document.createElement("p")
-main.append(section, aside2)
-section.append(h2, h3, p1, p2, p3, p4)
+main.append(section2, aside2)
+section2.append(h2, h3, p1, p2, p3, p4)
 aside2.append(div2, div3, div4)
 
+const li = document.createElement("li")
 
 
 input2.addEventListener("click", (event) => {
- event.preventDefault();
-let id = document.getElementById('text');
- let city = id.value.trim().split(' ').join('+')
- let url = `https://wttr.in/${city}?format=j1`;
-   
+    let id = document.getElementById('text');
+    let city = id.value.trim().split(' ').join('+')
+    let url = `https://wttr.in/${city}?format=j1`;
+    event.preventDefault();
 
     fetch(url)
-        .then((response) => response.json())
-        .then((resJson) => {
-            const nearArea = resJson.nearest_area[0]
-            const condition = resJson.current_condition[0]
-            const weather = resJson.weather[0]
-            const avgTemp1 = weather.avgtempF
-            // console.log(avgTemp1)
-            const maxTemp1 = weather.maxtempF
-            // console.log(maxTemp1)
-            const minTemp1 = weather.mintempF
-            // console.log(minTemp1)
-            const weather2 = resJson.weather[1]
-            const avgTemp2 = weather2.avgtempF
-            const maxTemp2 = weather2.maxtempF
-            const minTemp2 = weather2.mintempF
-            const weather3 = resJson.weather[2]
-            const avgTemp3 = weather3.avgtempF
-            const maxTemp3 = weather3.maxtempF
-            const minTemp3 = weather3.mintempF
-            const area = nearArea.areaName[0].value
-            const country = nearArea.country[0].value 
-            const region = nearArea.region[0].value
-            const temp = condition.FeelsLikeF
-
-
-
-            if(input2.textContent = area){
-                h3.textContent = area
-                p1.textContent = `Area: ${area}`
-                p2.textContent = `Country: ${country}`
-                p3.textContent = `Region: ${region}`
-                p4.textContent = `Currently feels like ${temp}`
-
-                pg1.textContent = `Avg Temp: ${avgTemp1}`
-                pg2.textContent = `Max Temp: ${maxTemp1}`
-                pg3.textContent = `Min Temp: ${minTemp1}`
-
-                page1.textContent = `Avg Temp: ${avgTemp2}`
-                page2.textContent = `Max Temp: ${maxTemp2}`
-                page3.textContent = `Min Temp: ${minTemp2}`
-
-                pa1.textContent = `Avg Temp: ${avgTemp3}`
-                pa2.textContent = `Max Temp: ${maxTemp3}`
-                pa3.textContent = `Min Temp: ${minTemp3}`
-            }
-
-            const anchor = document.createElement("a")
-            anchor.setAttribute("href", url)
-            anchor.textContent = `${area} - ${temp}`
-
-            const notes = anchor
-            generateList(notes)
-
-
+    .then((response) => response.json())
+    .then((resJson) => {
+        const nearArea = resJson.nearest_area[0]
+        const condition = resJson.current_condition[0]
+        const weather = resJson.weather[0]
+        const avgTemp1 = weather.avgtempF
+        // console.log(avgTemp1)
+        const maxTemp1 = weather.maxtempF
+        // console.log(maxTemp1)
+        const minTemp1 = weather.mintempF
+        // console.log(minTemp1)
+        const weather2 = resJson.weather[1]
+        const avgTemp2 = weather2.avgtempF
+        const maxTemp2 = weather2.maxtempF
+        const minTemp2 = weather2.mintempF
+        const weather3 = resJson.weather[2]
+        const avgTemp3 = weather3.avgtempF
+        const maxTemp3 = weather3.maxtempF
+        const minTemp3 = weather3.mintempF
+        const area = nearArea.areaName[0].value
+        const country = nearArea.country[0].value 
+        const region = nearArea.region[0].value
+        const temp = condition.FeelsLikeF
+        
+        
+        
+        
+            h3.textContent = area
+            p1.textContent = `Area: ${area}`
+            p2.textContent = `Country: ${country}`
+            p3.textContent = `Region: ${region}`
+            p4.textContent = `Currently feels like ${temp}`
             
-            form.reset()
+            pg1.textContent = `Avg Temp: ${avgTemp1}`
+            pg2.textContent = `Max Temp: ${maxTemp1}`
+            pg3.textContent = `Min Temp: ${minTemp1}`
+            
+            page1.textContent = `Avg Temp: ${avgTemp2}`
+            page2.textContent = `Max Temp: ${maxTemp2}`
+            page3.textContent = `Min Temp: ${minTemp2}`
+            
+            pa1.textContent = `Avg Temp: ${avgTemp3}`
+            pa2.textContent = `Max Temp: ${maxTemp3}`
+            pa3.textContent = `Min Temp: ${minTemp3}`
 
-         })
+            paragraph.classList.add("hidden")
+
+        const anchor = document.createElement("a")
+        
+        anchor.setAttribute("href", "javascript:history.go()")
+        anchor.textContent = `${area} - ${temp}`
+        
+        const notes = anchor
+        generateList(notes)
+        
+        
+        
+        
+        form.reset()
+    
+     
+    })
+    
     .catch((error) => console.log(error))
+
+
 })
 
 
-
 function addList(notes){
-    const li = document.createElement("li")
     li.textContent
     if(notes){
-        li.append(notes)
+        li.append(document.createElement("br"), notes)
     }
+    
     return li
 }
 
 
 function generateList(notes){
     const li = addList(notes);
-   const ul = document.querySelector("ul");
-    ul.append(li);
+    const ul = document.querySelector("ul");
+    
+        ul.append(li)
+    
+
 }
+
 
