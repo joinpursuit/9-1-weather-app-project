5ec7a6390e4d204b0a7762ff9029fe9345e587a6
PESolut <jahaadpetty@pursuit.org>
2022-09-03 02:34:30 -0400

    got api base url, used query selector to select header and article storage, added event listners to header form

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 console.log('main.js successfully linked')
+const BASE_URL = 'wttr.in'
+const JSON = '?format=j1'
+const form = document.querySelector('form')
+const artStor = document.querySelector('main aside')
+
+form.addEventListener('submit', (event)=>{
+    event.preventDefault()
+    console.log('form has been submitted successfully!')
+    console.log(event.value)
+})
+
+console.log(`${BASE_URL}${JSON}`)
+console.log(form)
+console.log(artStor)
+
+
+// LOG
+// 154 finished HTML exo earlier today, pushing: 'HTML skeleton completed'
+// getting api base url | line 2,3
+// printing to console, success !
+// select the header form | line 4
+// printing to console, success !
+// select the article storage ( in main ) | line 5
+// printing to console, success!
+// added event listener to header form, preventing default | line 7-8
+// printing to console on form submission, success !
+// saving input to variable
+// looking for where event variable stores input
