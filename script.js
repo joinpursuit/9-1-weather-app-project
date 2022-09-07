d9088b9ac0f1557cd112985ed4840d261d2b7d4a
NRiddick113 <NatashaRiddick@pursuit.org>
2022-09-07 12:31:06 -0400

    Weather-App

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ const lists =document.querySelector('ul')
 const click = document.querySelectorAll('aside section ul')
 const main = document.querySelector('main')
 const choose = document.querySelector('#choose')
-console.log(aside)
+const temperature = document.querySelector('#convert')
+const submitTemp = document.querySelector('aside form input')
+const submitWeather = document.querySelector('#getweather')
+let answers = document.querySelector('h4')
 
 const today = document.createElement('article')
 const tomorrow = document.createElement('article')
@@ -17,20 +20,42 @@ const p1 = document.createElement('p')
 const sunShine = document.createElement('p')
 const rain = document.createElement('p')
 const snow = document.createElement('p')
-// const previousSearch = document.querySelectorAll('ul li a')
-// console.log(click.childNodes)
- 
- 
- 
- form.addEventListener('submit', (event) => {
-     event.preventDefault()
+const sunPic = document.createElement('img')
+const rainPic = document.createElement('img')
+const snowPic = document.createElement('img')
 
-     let user = event.target['location'].value
 
-     city(user, true)
 
-     
-     event.target.reset()
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    
+    let user = event.target['location'].value
+    
+    city(user, true)
+    
+    
+    event.target.reset()
+})
+
+temperature.addEventListener('submit', (event) => {
+    event.preventDefault()
+    let temps = submitTemp.value
+    console.log(temps)
+    let answer = 0
+    let celsius = document.querySelector('#to-c')
+    
+        if(celsius.checked){
+          answer = (temps - 32) * 5/9
+          answer = (answer).toFixed(2) 
+        }else{
+            answer = (temps * 9/5) + 32
+            answer = (answer).toFixed(2) 
+        }
+        answers.innerText = ''
+        answers.append(answer)
+       
+        event.target.reset()
     })
     
     
@@ -39,11 +64,8 @@ const snow = document.createElement('p')
         fetch(`https://wttr.in/${user}?format=j1`)
         .then((res) => res.json())
         .then((res) => {
-            // console.log(res)
             const nearArea = res.nearest_area
-            // console.log(nearArea)
             nearArea.forEach(location => {
-                
                 area.innerText = `${user}`
                 
                 p1.innerText = `
@@ -52,6 +74,23 @@ const snow = document.createElement('p')
                 Country: ${location.country[0].value}
                 Currently: Feels Like ${res.current_condition[0].FeelsLikeF}°F`
                 article.append(area, p1)
+                if (res.weather[0].hourly[0].chanceofsunshine > 50){
+                    sunPic.setAttribute('src', './assets/icons8-summer.gif')
+                    sunPic.setAttribute('alt', 'sun')
+                    article.prepend(sunPic)
+                }else{
+                    sunPic.remove()
+                }
+                if (res.weather[0].hourly[0].chanceofrain > 50){
+                    rainPic.setAttribute('src', './assets/icons8-torrential-rain.gif')
+                    rainPic.setAttribute('alt', 'rain')
+                    article.prepend(rainPic)
+                }
+                if (res.weather[0].hourly[0].chanceofsnow > 50){
+                    snowPic.setAttribute('src', './assets/icons8-light-snow.gif')
+                    snowPic.setAttribute('alt', 'snow')
+                    article.prepend(snowPic)
+                }
                 
                 if (p){
                     p.remove()
@@ -59,22 +98,16 @@ const snow = document.createElement('p')
                 
                 choose.style.display = "none"
 
-                // convert Temp
-                const form = document.createElement('form')
-                const convert = document.createElement('label')
-                convert.innerText = 'Convert the temperature:'
-                const input = document.createElement('input')
-                input.setAttribute('type','number')
-                input.setAttribute('id', 'temp-to-convert')
-                aside.append(form)
-                
+
                 if(previousSearches) {
                     addSideBar(user, `${res.current_condition[0].FeelsLikeF}°F`)
                 }
                 
                 const days = res.weather
-                // console.log(days[0])
-                today.innerText = `Today\nAverage Temperature: ${days[0].avgtempF}°F\n Max Temperature: ${days[0].maxtempF}°F\nMin Temperature: ${days[0].mintempF}°F`
+                today.innerText = `Today
+                Average Temperature: ${days[0].avgtempF}°F
+                Max Temperature: ${days[0].maxtempF}°F
+                Min Temperature: ${days[0].mintempF}°F`
                 
                 tomorrow.innerText = `Tomorrow
                 Average Temperature: ${days[1].avgtempF}°F
@@ -87,7 +120,7 @@ const snow = document.createElement('p')
                 Max Temperature: ${days[2].maxtempF}°F
                 Min Temperature: ${days[2].mintempF}°F`
                 aside.append(today, tomorrow, dayAfter)
-                
+
             })
         })
         .catch((err) => {
@@ -107,41 +140,13 @@ const snow = document.createElement('p')
             fetch(`https://wttr.in/${cityName}?format=j1`)
         .then((res) => res.json())
         .then((res) => {
-
-            // console.log(res.weather[0].hourly[0].chanceofsunshine)
             sunShine.innerText =`Chance of Sunshine:${res.weather[0].hourly[0].chanceofsunshine}`
             rain.innerText =`Chance of Rain:${res.weather[0].hourly[0].chanceofrain}`
             snow.innerText =`Chance of Snow:${res.weather[0].hourly[0].chanceofsnow}`
             article.append(sunShine,rain,snow)
-
-            console.log(res.weather[0].hourly[0].chanceofsunshine)
-            if (res.weather[0].hourly[0].chanceofsunshine > 50){
-                const sun = document.createElement('img')
-                sun.setAttribute('src', './assets/icons8-summer.gif')
-                sun.setAttribute('alt', 'sun')
-                article.prepend(sun)
-            }
-            
-            // if (res.weather[0].hourly[0].chanceofrain > 50){
-            //     const rain = document.createElement('img')
-            //     rain.setAttribute('src', './assets/icons8-torrential-rain.gif')
-            //     rain.setAttribute('alt', 'rain')
-            //     article.prepend(rain)
-            // }
-            
-            // if (res.weather[0].hourly[0].chanceofsnow > 50){
-            //     const snow = document.createElement('img')
-            //     snow.setAttribute('src', './assets/icons8-light-snow.gif')
-            //     snow.setAttribute('alt', 'snow')
-            //     article.prepend(snow)
-
-            // }
-
-
                 })
         .catch((err) => {
          console.log(err)
         })
     })
 }
-    
