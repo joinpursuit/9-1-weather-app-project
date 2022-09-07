8320b67fca2b9c6d828f8bcbf46cc6598a234028
Lennie Nurse <lnursej@gmail.com>
2022-09-06 22:29:40 -0400

    "completed 3 forecast days"

--- main.js
+++ main.js
@@ -14,13 +14,40 @@ form.addEventListener("submit", (event) => {
 
       document.querySelector("article.location").innerHTML = `
      <h2>${searchLocation}</h2>
-     <p>Nearest Area::${resJson.nearest_area[0].areaName[0].value}</p>
-     <p>Region:${resJson.nearest_area[0].region[0].value}</p> 
-     <p>Country:${resJson.nearest_area[0].country[0].value}</p>
-     <p>Currently:Feels Like${resJson.current_condition[0].FeelsLikeF}°F</p>
-     <p>Chance of Sunshine:${resJson.weather[0].hourly[0].chanceofsunshine}</p>
-     <p>Chance of Rain:${resJson.weather[0].hourly[0].chanceofrain}</p>
-     <p>Chance of Snow:${resJson.weather[0].hourly[0].chanceofsnow}</p>`;
+     <p>Nearest Area: ${resJson.nearest_area[0].areaName[0].value}</p>
+     <p>Region: ${resJson.nearest_area[0].region[0].value}</p> 
+     <p>Country: ${resJson.nearest_area[0].country[0].value}</p>
+     <p>Currently:Feels Like ${resJson.current_condition[0].FeelsLikeF}°F</p>
+     <p>Chance of Sunshine: ${resJson.weather[0].hourly[0].chanceofsunshine}</p>
+     <p>Chance of Rain: ${resJson.weather[0].hourly[0].chanceofrain}</p>
+     <p>Chance of Snow: ${resJson.weather[0].hourly[0].chanceofsnow}</p>`;
+
+      // listing today weather average, max and min temperature
+      document.querySelector("article.Today").innerHTML = `
+      <h3>Today</h3>
+      <p>Average Temperature: ${resJson.weather[0].avgtempF}°F</p>
+      <p>Max Temperature: ${resJson.weather[0].maxtempF}°F</p>
+      <p>Min Temperature: ${resJson.weather[0].mintempF}°F</p>`;
+
+      // listing tomorrow weather average, max and min temperature
+      document.querySelector("article.Tomorrow").innerHTML = `
+      <h3>Tomorrow</h3>
+      <p>Average Temperature: ${resJson.weather[1].avgtempF}°F</p>
+      <p>Max Temperature: ${resJson.weather[1].maxtempF}°F</p>
+      <p>Min Temperature: ${resJson.weather[1].mintempF}°F</p>`;
+
+      // listing Day after tomorrow average, max and min temperature
+      document.querySelector("article.AfterTomorrow").innerHTML = `
+      <h3>Day After Tomorrow</h3>
+      <p>Average Temperature: ${resJson.weather[2].avgtempF}°F</p>
+      <p>Max Temperature: ${resJson.weather[2].maxtempF}°F</p>
+      <p>Min Temperature: ${resJson.weather[2].mintempF}°F</p>`;
+
+      aside1.location.append(
+        article.Today,
+        article.tomorrow,
+        article.AfterTomrrow
+      );
 
       //  ICON based on data changes
       const img = document.createElement("img");
@@ -40,11 +67,22 @@ form.addEventListener("submit", (event) => {
         img.setAttribute("src", "./assets/icons8-light-snow.gif");
         img.setAttribute("alt", "snow");
       }
+      if (condition) {
+      }
+      main.append(img);
 
       // WIDGET - Temperature conversion
 
-      const aside1 = document.querySelector("aside");
-      aside1.after(a);
+      const aside1 = document.querySelector("aside.Conversion");
+      const label1 = document.createElement("label");
+      aside1.append(label1);
+      const form1 = document.createElement("form");
+      label1.append(form1);
+
+      form.addEventListener("submit", (event) => {
+        event.preventDefault();
+      });
+
       //   const a1 = doucment.querySelector("article");
       //   a1.innerText = "Today";
     })
