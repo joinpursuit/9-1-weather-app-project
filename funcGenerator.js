431980e3cbadbc69e6ded917c2152729238a444b
Amina-Moufakkir <aminamoufakkir@pursuit.org>
2022-09-09 00:49:46 -0400

    add styling

--- funcGenerator.js
+++ funcGenerator.js
@@ -1,47 +1,45 @@
 function getWeatherData(data) {
     const { nearest_area: [ { areaName: [ {value: area} ]  }  ] }  = data;
 
-    const { nearest_area: [ { country: [ {value: country} ] } ] } = data;
+    const { nearest_area: [ { country: [ { value: country} ] } ] } = data;
     
-      const { nearest_area: [ { region: [ {value: region} ] } ] } = data;
+    const { nearest_area: [ { region: [ { value: region} ] } ] } = data;
     
-      const { current_condition: [ { FeelsLikeF: currentFeelsLikeF } ] } = data;
+    const { current_condition: [ { FeelsLikeF: currentFeelsLikeF } ] } = data;
     
-      const { weather: [ { hourly } ] } = data;
+    const { weather: [ { hourly } ] } = data;
     
-      let todayWeather = {
+    let todayWeather = {
       currentDate: "Today",
       avgTempF: data.weather[0].avgtempF,
       maxTempF: data.weather[0].maxtempF,
       minTempF: data.weather[0].mintempF,
       };
-      let tomorrowWeather = {
+    let tomorrowWeather = {
       currentDate: "Tomorrow",
       avgTempF: data.weather[1].avgtempF,
       maxTempF: data.weather[1].maxtempF,
       minTempF: data.weather[1].mintempF,
       };
-      let threeDaysWeather = {
+    let threeDaysWeather = {
       currentDate: "Day After Tomorrow",
       avgTempF: data.weather[2].avgtempF,
       maxTempF: data.weather[2].maxtempF,
       minTempF: data.weather[2].mintempF,
       };
 
-      weatherData = {
-      area,
-      region,
-      country,
-      currentFeelsLikeF,
-      hourly,
-      threeDaysForcast: [
-          todayWeather,
-          tomorrowWeather,
-          threeDaysWeather,
-      ],
+      return weatherData = {
+        area,
+        region,
+        country,
+        currentFeelsLikeF,
+        hourly,
+        threeDaysForcast: [
+            todayWeather,
+            tomorrowWeather,
+            threeDaysWeather,
+        ],
       };
-
-      return weatherData;
   }
 
 
@@ -51,12 +49,16 @@ function displayWeather(userInput, currentWeather,weatherData) {
 let location = document.createElement("h2");
 location.innerHTML = userInput;
 currentWeather.append(location);
+location.style.cssText = "text-transform: capitalize; color: #eb4747; text-decoration: underline #e8aa42; text-underline-offset: 5px;"
 
 let area = document.createElement("p");
+
   area.innerHTML =
   userInput.toLowerCase() === weatherData.area.toLowerCase() ?  `<strong>Area:</strong> ${(weatherData.area)}`
   : `<strong>Nearest Area:</strong> ${weatherData.area}`;
+ 
 currentWeather.append(area);
+
 
 let region = document.createElement("p");
 region.innerHTML = `<strong>Region:</strong> ${weatherData.region}`;
@@ -125,11 +127,13 @@ if (highestChanceOfSunshine > 50) {
 if (highestChanceOfRain > 50) {
   icon.src = "./assets/icons8-torrential-rain.gif";
   icon.alt = "rain";
+  icon.style="width: 100px; margin-top: 20px;"
   background__img.style.backgroundImage = "url('./my-images/rainy-day.jpeg')";
 }
 if (highestChanceOfSnow > 50) {
   icon.alt = "snow";
   icon.src = "./assets/icons8-light-snow.gif";
+  
   background__img.style.backgroundImage = "url('./my-images/snowy-day.jpeg')";
 } else {
   background__img.backgroundImage = 'url("./my-images/blue-sky.jpg")';
@@ -170,11 +174,14 @@ currentWeather.prepend(icon);
       let currentFeelsLikeF = weatherData.currentFeelsLikeF;
       
       let li = document.createElement("li");
+      li.style = "list-style: none; font-size:12px;"
       let a = document.createElement("a");
+      
       a.textContent = location;
+      a.style="text-transform: capitalize;font-size:14px;"
       a.href = `${BASE_URL}${location}?format=j1`
       
-      li.textContent = ` - ${currentFeelsLikeF}°F `;
+      li.textContent = ` ${currentFeelsLikeF}°F `;
       li.prepend(a);
     
       let seachHistoryList = document.querySelector("ul");
@@ -202,9 +209,9 @@ currentWeather.prepend(icon);
       });
     
       let removeBtn = document.createElement("button");
-      removeBtn.innerText = "Remove";
+      removeBtn.innerHTML = ` <i class="fa-solid fa-trash"></i>`
       li.append(removeBtn);
-      removeBtn.style.cssText += "border-radius:5px; background-color:lightgray; height: 25px; border: none;";
+      removeBtn.style.cssText = "border-radius:5px; background-color:#EB5406; height: 25px; border: none;margin:5px;";
       removeBtn.addEventListener("click", () => {
         li.remove();
       });
