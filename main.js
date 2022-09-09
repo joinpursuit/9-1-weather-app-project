de7ee0b29a6ba215a2b61c9612f2a43ea8f995bd
ronJohnPursuit <ronjohnson@pursuit.org>
2022-09-09 13:15:57 -0400

    pass

--- main.js
+++ main.js
@@ -78,14 +78,77 @@ weatherForm.addEventListener("submit", (eve) => {
       });
 
     prevSearch.innerText = "";
-    prevSearchList = document.createElement("ul");
+    const prevSearchList = document.createElement("ul");
     searchHistory.append(prevSearchList);
 
-    lastSearch = document.createElement("li");
-    lastSearch.innerHTML = `<a href="#">${postTitle.value}</a>`;
-    lastSearch.addEventListener("click", (eve) => {
-      // hhheellpp
+    const lastSearch = document.createElement("li");
+    const lastSearchAnchor = document.createElement("a");
+    lastSearchAnchor.innerText = postTitle.value;
+    lastSearch.append(lastSearchAnchor);
+
+    lastSearchAnchor.addEventListener("click", (eve) => {
+      eve.preventDefault();
+
+      fetch(url)
+        .then((res) => res.json())
+        .then((res) => {
+          console.log(`Feels Like ${res.current_condition[0]["FeelsLikeF"]}°F`);
+          // code the web page repopulation
+          console.log(res.nearest_area[0].areaName[0].value);
+          console.log(res.nearest_area[0].region[0].value);
+          console.log(res.nearest_area[0].country[0].value);
+          asideMain = document.createElement("aside");
+          mainContent.innerHTML = "";
+          mainContent.innerHTML = `<h2 style="color: blue">${eve.target.innerText}</h2>`;
+
+          mainContent.append(asideMain);
+          articleArea = document.createElement("article");
+          asideMain.append(articleArea);
+          articleArea.innerText = `Area: ${res.nearest_area[0].areaName[0].value}`;
+
+          articleRegion = document.createElement("article");
+          asideMain.append(articleRegion);
+          articleRegion.innerText = `Region: ${res.nearest_area[0].region[0].value}`;
+          articleCountry = document.createElement("article");
+          asideMain.append(articleCountry);
+          articleCountry.innerText = `Country: ${res.nearest_area[0].country[0].value}`;
+          articleCurrently = document.createElement("article");
+          asideMain.append(articleCurrently);
+          articleCurrently.innerText = `Currently: Feels Like ${res.current_condition[0]["FeelsLikeF"]}°F`;
+
+          console.log(res);
+          console.log(`${Object.keys(res.current_condition[0])[0]}`);
+          //
+          asideMainFuture = document.createElement("aside");
+          // asideMainFuture.innerText = "Today ";
+          asideMainFuture.style =
+            "   display: grid; grid-template-columns: 1fr 1fr 1fr";
+          // asideMainFuture.style = "grid-column: 1/2";
+          mainContent.append(asideMainFuture);
+          //
+          asideMainToday = document.createElement("article");
+
+          asideMainToday.innerHTML = `Today: <br> Average Temperature: ${res.weather[0]["avgtempF"]}°F <br> Max Temperature: ${res.weather[0]["maxtempF"]}°F <br> Min Temperature: ${res.weather[0]["mintempF"]}°F`;
+
+          asideMainToday.style = "grid-column: 1/1; color: black";
+          asideMainFuture.append(asideMainToday);
+          //
+          asideMainTomorrow = document.createElement("article");
+
+          asideMainTomorrow.innerHTML = `Tomorrow: <br> Average Temperature: ${res.weather[1]["avgtempF"]}°F <br> Max Temperature: ${res.weather[1]["maxtempF"]}°F <br> Min Temperature: ${res.weather[1]["mintempF"]}°F`;
+
+          asideMainTomorrow.style = "grid-column: 2/2; color: black";
+          asideMainFuture.append(asideMainTomorrow);
+          //
+          asideMainDayAfter = document.createElement("article");
+
+          asideMainDayAfter.innerHTML = `Day After Tomorrow: <br> Average Temperature: ${res.weather[2]["avgtempF"]}°F <br> Max Temperature: ${res.weather[2]["maxtempF"]}°F <br> Min Temperature: ${res.weather[2]["mintempF"]}°F`;
+
+          asideMainDayAfter.style = "grid-column: 3/3; color: black";
+          asideMainFuture.append(asideMainDayAfter);
+        });
     });
+
     prevSearchList.appendChild(lastSearch);
     console.log(lastSearch.innerText);
     //
